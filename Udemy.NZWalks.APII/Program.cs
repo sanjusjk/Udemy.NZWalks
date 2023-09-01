6beb2ce6506e1256c7eb32cf8039c453329246d3
Sanjeev Jyothikrishnan E <sanju.jyothikrishnan@gmail.com>
2023-09-01 11:31:56 +0530

    Changes from udemy course section 2

--- Udemy.NZWalks.APII/Program.cs
+++ Udemy.NZWalks.APII/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Udemy.NZWalks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//used to inject DB Context class
+builder.Services.AddDbContext<NZWalksDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Conn")));
 
 var app = builder.Build();
 
